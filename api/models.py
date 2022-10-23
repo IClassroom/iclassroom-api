c02f76bd9d5d232fa2f52de0423af4d161bec207
souzag <glls@ic.ufal.br>
2022-10-23 16:43:55 -0300

    feat: modelos da aula e cronograma

--- api/models.py
+++ api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Cronograma(models.Model):
+    id = models.AutoField(
+        primary_key = True
+    )
+
+    turma_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'iclassroom\".\"cronograma'
+        verbose_name_plural = 'Cronogromas'
+
+class Aula(models.Model):
+    id = models.AutoField(
+        primary_key = True
+    )
+
+    cronograma_id = models.ForeignKey(
+        Cronograma, 
+        on_delete = models.CASCADE,
+        db_column = 'cronograma_id'
+    )
+
+    dia = models.DateField()
+
+    titulo = models.CharField(
+        max_length = 250
+    )
+
+    assuntos = models.CharField(
+        max_length = 500
+    )
+
+    descricao = models.CharField(
+        max_length = 700
+    )
+
+    class Meta:
+        db_table = 'iclassroom\".\"aula'
+        verbose_name_plural = 'Aulas'
