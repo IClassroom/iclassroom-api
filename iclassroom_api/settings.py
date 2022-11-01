f8278fcea38f72603f657c03e9688b4c4df169c3
raksantos <rss3@ic.ufal.br>
2022-11-01 12:15:44 -0300

    feat: adicionando testes funcionais para usuário

--- iclassroom_api/settings.py
+++ iclassroom_api/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1o-85ii8hcpoirz+y)h3&5p4k1b9-52r%66ckbu9m^ht86iq+#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(",")
 
 CORS_ALLOW_METHODS = [
     'DELETE',
@@ -35,6 +35,10 @@ CORS_ALLOW_METHODS = [
     'POST',
     'OPTIONS'
     'PUT',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 CORS_ALLOW_HEADERS = [
@@ -118,7 +122,10 @@ DATABASES = {
         'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
         'PORT': config('DATABASE_PORT'),
-    }
+    },
+    'OPTIONS': {
+        'options': '-c search_path=django, public, iclassroom',
+    },
 }
 
 
@@ -145,13 +152,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
-
 TIME_ZONE = 'America/Maceio'
-
 USE_I18N = True
-
 USE_TZ = True
-
 DECIMAL_SEPARATOR = ','
 DECIMAL_PLACES = 2
 THOUSAND_SEPARATOR = '.'
--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -1,78 +1,92 @@
 from django.test import TestCase, RequestFactory
-from django import setup
 from rest_framework import status
-import os
-import json
+from django import setup
+import os, json
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iclassroom_api.settings')
 setup()
 
 class UsuarioTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+        self.base_route = "/api/usuario/"
+        self.login_route = "/api/login/"
+
         self.test_user = {
-            "nome": "Rodrigo Santos da Silva",
-            "email": "rss3@ic.ufal.br",
+            "nome": "Teste",
+            "email": "teste@gmail.com",
             "password": "123456"
         }
 
-        self.base_route = '/api/usuario/'
+        self.login_user = {
+            "email": "teste@gmail.com",
+            "password": "123456"
+        }
 
-    def test_should_get_all_users(self):
-        """Testa se o endpoint de listagem de usuarios retorna todos os usuarios"""
-        response = self.client.get(self.base_route)
+    def authenticate(self):
+        """
+        Função auxiliar para realizar autenticação do usuário nos testes
+
+        Retorna:
+
+        Token: Token do usuário
+        Id: Id do usuário
+        """
+        user = self.client.post(self.base_route, self.test_user)
+
+        response = self.client.post(self.login_route, self.login_user)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        return response.data['token'], user.data
 
-    def test_should_create_test_user(self):
-        """Testa se o endpoint de criação de curso cria e retorna o curso criado"""
-
-        response = self.client.post(self.base_route, self.test_user, format='json')
+    def test_should_create_user(self):
+        """
+        Testa a criação de um usuário e verifica se o status code foi 201
+        """
+        response = self.client.post(self.base_route, self.test_user)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_should_get_test_user_by_id(self):
-        """Testa se o endpoint de listagem de cursos retorna o curso especificado"""
 
-        response = self.client.post(self.base_route, self.test_user, format='json')
-        response = self.client.get(f'{self.base_route}{response.data["id"]}/'.format(response.data['id']))
+    def test_should_get_all_users(self):
+        """
+        Testa a listagem de todos os usuários e verifica se o status code foi 200
+        """
+        token, user = self.authenticate()
+        response = self.client.get(self.base_route, HTTP_AUTHORIZATION='Token ' + token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_should_get_user_by_id(self):
+        """
+        Testa a listagem de um usuário específico e verifica se o status code foi 200
+        """
+        token, data = self.authenticate()
+        response = self.client.get(f'{self.base_route}{data["id"]}/', HTTP_AUTHORIZATION='Token ' + token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_should_update_user(self):
-        """Testa se o endpoint de atualização de curso atualiza e retorna o curso atualizado"""
+        """
+        Testa a atualização de um usuário e verifica se o status code foi 200
+        """
+        token, data = self.authenticate()
+        data["nome"] = "Teste 2"
+        data["password"] = self.test_user["password"]
 
-        response = self.client.post(self.base_route, self.test_user, format='json')
+        user = json.dumps(data)
 
-        self.test_user["carga_horaria"] = 200
-
-        user = json.dumps(self.test_user)
-
-        response = self.client.put(f'{self.base_route}{response.data["id"]}/', data=user, content_type='application/json')
-
+        response = self.client.put(f'{self.base_route}{data["id"]}/', data=user, content_type='application/json',
+                                    HTTP_AUTHORIZATION='Token ' + token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_should_update_specific_field(self):
-        """Testa se o endpoint de atualização de curso atualiza o campo específico e retorna o curso atualizado"""
-
-        response = self.client.post(self.base_route, self.test_user)
-
-        self.test_user["titulo"] = "Novo título"
-
-        user = json.dumps(self.test_user)
-
-        response = self.client.put(f'{self.base_route}{response.data["id"]}/', data=user, content_type='application/json')
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_should_return_404_for_not_found_user(self):
-        """Testa se o endpoint de atualização de curso retorna 404 para um curso não encontrado"""
-        response = self.client.get(f'{self.base_route}1a/')
-
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_should_remove_user(self):
-        """Testa se o endpoint de remoção de curso remove e retorna 204"""
-
-        response = self.client.post(self.base_route, self.test_user, format='json')
-
-        response = self.client.delete(f'{self.base_route}{response.data["id"]}/')
-
+    def test_should_delete_user(self):
+        """
+        Testa a deleção de um usuário e verifica se o status code foi 204
+        """
+        token, data = self.authenticate()
+        response = self.client.delete(f'{self.base_route}{data["id"]}/', HTTP_AUTHORIZATION='Token ' + token)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_should_return_404_when_user_not_found(self):
+        """
+        Testa a listagem de um usuário inexistente e verifica se o status code foi 404
+        """
+        token, user = self.authenticate()
+        response = self.client.get(f'{self.base_route}0/', HTTP_AUTHORIZATION='Token ' + token)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
