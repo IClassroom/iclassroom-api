c7c6cd474ccbeb0d4b58f4a6ec6c9e325df4b820
raksantos <rss3@ic.ufal.br>
2022-11-29 11:55:18 -0300

    feat: finalizando testes

--- tests/test_atividades.py
+++ tests/test_atividades.py
@@ -95,117 +95,13 @@ class TopicosTesteCase(TestCase):
         response = self.client.post(f"{self.base_route}", self.test_activity, HTTP_AUTHORIZATION='Token ' + token)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_topic(self):
-        """
-        Teste para criação de um tópico
-        """
+    def test_get_activity_by_id(self):
         token, user, class_id = self.create_class_and_authenticate()
         self.test_topic['turma_id'] = class_id
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
+        self.test_activity['turma_id'] = class_id
+        response = self.client.post(self.create_topic_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
+        self.test_activity["topico_id"] = response.data['id']
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_get_all_topics(self):
-        """
-        Teste para listagem de todos os tópicos
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = class_id
-        self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
-        response = self.client.get(self.base_route, HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_topic_by_id(self):
-        """
-        Teste para listagem de um tópico específico
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = class_id
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post(f"{self.base_route}", self.test_activity, HTTP_AUTHORIZATION='Token ' + token)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response = self.client.get(f"{self.base_route}{response.data['id']}/", HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_update_topic(self):
-        """
-        Teste para atualização de um tópico
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = class_id
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-        self.test_topic['titulo'] = "Teste 2"
-        self.test_topic['descricao'] = "Teste 2"
-
-        topic = json.dumps(self.test_topic)
-
-        response = self.client.put(f"{self.base_route}{response.data['id']}/", data = topic, HTTP_AUTHORIZATION='Token ' + token,
-                                   content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_should_update_specific_item_from_topic(self):
-        """
-        Teste para atualização de um item espeífico de tópico
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = class_id
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-        self.test_topic['titulo'] = "Teste 2"
-
-        topic = json.dumps(self.test_topic)
-
-        response = self.client.patch(f"{self.base_route}{response.data['id']}/", data = topic, HTTP_AUTHORIZATION='Token ' + token,
-                                   content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_delete_topic(self):
-        """
-        Teste para deleção de um tópico
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = class_id
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        response = self.client.delete(f"{self.base_route}{response.data['id']}/", HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    def test_should_not_create_topic_with_invalid_class_id(self):
-        """
-        Teste para criação de um tópico com id de turma inválido
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = 100
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_should_not_get_topic_with_invalid_id(self):
-        """
-        Teste para listagem de um tópico com id inválido
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = class_id
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        response = self.client.get(f"{self.base_route}100/", HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_should_not_update_topic_with_invalid_id(self):
-        """
-        Teste para atualização de um tópico com id inválido
-        """
-        token, user, class_id = self.create_class_and_authenticate()
-        self.test_topic['turma_id'] = class_id
-        response = self.client.post(self.base_route, self.test_topic, HTTP_AUTHORIZATION='Token ' + token)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-        self.test_topic['titulo'] = "Teste 2"
-        self.test_topic['descricao'] = "Teste 2"
-
-        topic = json.dumps(self.test_topic)
-
-        response = self.client.put(f"{self.base_route}100/", data = topic, HTTP_AUTHORIZATION='Token ' + token,
-                                   content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
