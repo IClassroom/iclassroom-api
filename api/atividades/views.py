bba72e1340dfd99353206e4e6c3903d10dc60cc8
raksantos <rss3@ic.ufal.br>
2022-11-29 11:48:43 -0300

    chore: correções minimas e início dos testes

--- api/atividades/views.py
+++ api/atividades/views.py
@@ -3,6 +3,6 @@ from rest_framework import viewsets
 from api.models import Atividade
 from .serializers import AtividadeSerializer
 
-class AtividadeSerializer(viewsets.ModelViewSet):
+class AtividadeView(viewsets.ModelViewSet):
     queryset = Atividade.objects.all()
     serializer_class = AtividadeSerializer
--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,13 @@ from .usuario.views import UsuarioView, login
 from .turmas.views import TurmaView
 from .topicos.views import TopicoView
 from .avisos.views import AvisoView
+from .atividades.views import AtividadeView
 
 router = DefaultRouter()
 router.register(r'usuario', UsuarioView, basename='usuario')
 router.register(r'turma', TurmaView, basename='turma')
 router.register(r'topico', TopicoView, basename='topico')
 router.register(r'aviso', AvisoView, basename='aviso')
+router.register(r'atividade', AtividadeView, basename='atividade')
 urlpatterns = router.urls
 urlpatterns.append(path('login/', login))
